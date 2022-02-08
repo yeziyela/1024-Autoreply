3873544ecc503d95c3a06de9985695a3b1b13803
honus <63273720+0honus0@users.noreply.github.com>
2022-02-08 11:49:44 +0800

    Update 1024.py

--- 1024.py
+++ 1024.py
@@ -183,7 +183,7 @@ class Autoreply:
     def getreply():
         #自定义回复内容，记得修改随机数
         reply=['感谢分享','感谢你的分享','谢谢分享','多谢分享','感谢作者的分享','谢谢坛友分享','内容精彩','的确如此','感谢分享','涨知识了','很有意思']
-        reply_m=random.randint(0,len(reply))
+        reply_m=random.randint(0,len(reply)-1)
         reply_news=reply[reply_m]
         print('本次回复消息是:'+reply_news)
         return  reply_news
